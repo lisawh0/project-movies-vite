17ae6d7e53765f649b7ce627421c3cc518d116e4
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-17 18:30:32 +0200

    terminal wants me to commit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import "./movielist.css";
 import MovieCard from './MovieCard';
 
 function MovieList() {

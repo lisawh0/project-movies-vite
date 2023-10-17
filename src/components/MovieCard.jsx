ee295639ab45e80165531828585c95ca803cf7b9
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-17 18:13:34 +0200

    saving

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./movielist.css";
 
 function MovieCard({ movie }) {
     return (
         <div className="movie-card">
-            <img
-                src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-                alt={movie.title}
-            />
-            <div className="movie-title">{movie.title}</div>
-
+            <Link to={`/Details/${movie.id}`}> {/* Dynamic link with movie ID */}
+                <img
+                    src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                    alt={movie.title}
+                />
+                <div className="movie-title">{movie.title}</div>
+            </Link>
         </div>
     );
 }

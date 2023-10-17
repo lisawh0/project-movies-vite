41baf0b23826cbc552efbcf1c38cd82efef14f56
jforjanice <jjanice.cheng@gmail.com>
2023-10-17 16:46:34 +0200

    added route detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MovieList } from "./components/MovieList";
+import { Detail } from "./components/Detail";
 
 export const App = () => {
-
   return (
-
     <BrowserRouter>
       <div className="main-wrapper">
-        <Routes >
+        <Routes>
           <Route path="/" element={<MovieList />} />
+          <Route path="/" element={<Detail />} />
         </Routes>
       </div>
     </BrowserRouter>
-
   );
 };
